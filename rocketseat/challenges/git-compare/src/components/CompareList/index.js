f7d8d9af02e7730bd02fbded3c87e31d476aedd4
Bruno Scherer <bruno.raphael96@gmail.com>
2019-04-13 13:01:23 -0300

    add: second challenge

---
+++ rocketseat/challenges/git-compare/src/components/CompareList/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container, Repository } from './styles';
+
+const CompareList = ({ repositories, removeRepository, updateRepository }) => (
+  <Container>
+    {repositories.map(repository => (
+      <Repository key={repository.id}>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
+
+        <ul>
+          <li>
+            {repository.stargazers_count}
+            {' '}
+            <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count}
+            {' '}
+            <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count}
+            {' '}
+            <small>issues</small>
+          </li>
+          <li>
+            {repository.lastCommit}
+            {' '}
+            <small>last commit</small>
+          </li>
+        </ul>
+
+        <section className="button__container">
+          <button type="button" onClick={() => updateRepository(repository.id)}>
+            <span className="fa fa-retweet" />
+            Update
+          </button>
+
+          <button type="button" onClick={() => removeRepository(repository.id)}>
+            <span className="fa fa-trash" />
+            Remove
+          </button>
+        </section>
+      </Repository>
+    ))}
+  </Container>
+);
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string,
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string,
+    }),
+  ).isRequired,
+  removeRepository: PropTypes.func.isRequired,
+  updateRepository: PropTypes.func.isRequired,
+};
+
+export default CompareList;
---
+++ rocketseat/challenges/git-compare/src/pages/Main/index.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.png';
+
+import { Container, Form } from './styles';
+
+import CompareList from '../../components/CompareList';
+
+export default class Main extends Component {
+  state = {
+    loading: false,
+    repositoryError: false,
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    this.setState({ loading: false, repositories: await this.getLocalRepositories() });
+  }
+
+  getLocalRepositories = async () => JSON.parse(await localStorage.getItem('repositories')) || [];
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { repositoryInput, repositories } = this.state;
+
+    try {
+      const { data: repository } = await api.get(`repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+        repositoryError: false,
+      });
+
+      const localRepositories = await this.getLocalRepositories();
+
+      await localStorage.setItem(
+        'repositories',
+        JSON.stringify([...localRepositories, repository]),
+      );
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  handleRemoveRepository = async (id) => {
+    const { repositories } = this.state;
+
+    const updatedRepositories = repositories.filter(repository => repository.id !== id);
+
+    this.setState({
+      repositories: updatedRepositories,
+    });
+
+    await localStorage.setItem('repositories', JSON.stringify(updatedRepositories));
+  };
+
+  handleUpdateRepository = async (id) => {
+    const { repositories } = this.state;
+
+    const repository = repositories.find(repo => repo.id === id);
+
+    try {
+      const { data } = await api.get(`repos/${repository.full_name}`);
+
+      data.lastCommit = moment(data.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: repositories.map(repo => (repo.id === data.id ? data : repo)),
+        repositoryError: false,
+      });
+
+      await localStorage.setItem('repositories', JSON.stringify(repositories));
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    }
+  };
+
+  render() {
+    const {
+      repositories, repositoryInput, repositoryError, loading,
+    } = this.state;
+
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="user/repository"
+            value={repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">
+            {loading ? <span className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
+        </Form>
+
+        <CompareList
+          repositories={repositories}
+          removeRepository={this.handleRemoveRepository}
+          updateRepository={this.handleUpdateRepository}
+        />
+      </Container>
+    );
+  }
+}
