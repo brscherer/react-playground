a91a8f646c9892d290202750e5a0f5e8ee05c161
Bruno Scherer <bruno.raphael96@gmail.com>
2026-02-02 22:29:32 -0300

    fetch-vs-axios: remove msw

--- fetch-vs-axios/src/mocks/browser.ts
+++
@@ -1,9 +0,0 @@
-import { setupWorker } from 'msw';
-import { handlers } from './handlers';
-
-export const worker = setupWorker(...handlers);
-
-export async function startMockServiceWorker() {
-  if (typeof window === 'undefined') return;
-  await worker.start({ onUnhandledRequest: 'bypass' });
-}
