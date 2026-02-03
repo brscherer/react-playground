83eb3b25b5a4b8a63e54e4afad2297fe583dcded
Bruno Scherer <bruno.raphael96@gmail.com>
2026-02-02 22:02:53 -0300

    fetch-vs-axios: install msw

--- fetch-vs-axios/src/main.tsx
+++ fetch-vs-axios/src/main.tsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function init() {
+  if (import.meta.env.DEV) {
+    try {
+      const { startMockServiceWorker } = await import('./mocks/browser');
+      await startMockServiceWorker();
+      console.log('MSW: mock service worker started');
+    } catch (e) {
+      console.warn('MSW failed to start', e);
+    }
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+init();
