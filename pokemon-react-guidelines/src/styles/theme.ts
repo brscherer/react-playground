57e524d6c7dd5c105b12afbf3b66e2d76b238895
Bruno Scherer <bruno.raphael96@gmail.com>
2026-03-11 19:58:50 -0700

    pokemon-react-guidelines: fix styles

---
+++ pokemon-react-guidelines/src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  background: "#f6f8fc",
+
+  card: "#ffffff",
+
+  pastelRed: "#ffadad",
+  pastelBlue: "#a0c4ff",
+  pastelYellow: "#fdffb6",
+  pastelGreen: "#caffbf",
+  pastelPurple: "#bdb2ff",
+
+  text: "#333",
+}
